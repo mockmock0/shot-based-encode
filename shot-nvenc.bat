f0dcd3fb34a464581c588549bd2232a15cee4d20
mockmock <149362146+mockmock0@users.noreply.github.com>
2024-07-26 17:45:41 +0900

    Update shot-nvenc.bat
    fix framerate info error

--- shot-nvenc.bat
+++ shot-nvenc.bat
@@ -1,4 +1,4 @@
-REM Usage : shot-nvenc.bat [fileName] [interpolate] [encoder] [preset] [vmaf]
+REM Usage : auto.bat [fileName] [interpolate] [encoder] [preset] [vmaf]
 
 @echo off
 chcp 65001
@@ -7,7 +7,11 @@ set dt=%DATE:~6,4%_%DATE:~3,2%_%DATE:~0,2%__%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
 set current_date=%dt: =0%
 mkdir %current_date%_splitted_video
 ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate %1 > fps.txt
-for /f "tokens=*" %%a in ( fps.txt ) do set fr=%%a
+for /f "usebackq delims=" %%A in ("fps.txt") do (
+    set fr=%%A
+    goto :DONE
+)
+:DONE
 for /f "tokens=*" %%A in ('powershell -command "( !fr!*%2 )"') do set tf=%%A
 del "fps.txt"
 if %2==1 (
@@ -15,7 +19,7 @@ if %2==1 (
 ) else (
     SET /a isInterpolate=1
 )
-scenedetect -i %1 detect-adaptive -m 1.5 split-video -a "-an -vcodec hevc_nvenc -preset slow -qp 5" -o %current_date%_splitted_video
+scenedetect -i %1 detect-adaptive -m 1.5 split-video -a "-an -vcodec hevc_nvenc -qp 5 -preset slow" -o %current_date%_splitted_video
 echo.
 echo Extracting Audio ...
 ffmpeg -i %1 -vn -acodec libopus -b:a 96k "%current_date%_splitted_video/%~n1.opus" 2> NUL
@@ -44,7 +48,7 @@ for %%f in (*.mp4) do (
         cd ..
         rife-ncnn-vulkan -i input_frames/ -n !count! -m rife-v4.18 -o output_frames/ 2> NUL
         ffmpeg -r %tf% -i output_frames/%%08d.png -vcodec hevc_nvenc -preset slow -qp 5 -pix_fmt yuv420p10le "%%~nf_prerendered.mp4" 2> NUL
-        echo Analyzing File: %%f
+        echo Adjusting File: %%f
         SET /a crf_value = 0
         ab-av1 crf-search -i "%%~nf_prerendered.mp4" -e %enc% --preset %pre% --min-vmaf %5 --max-crf 45 --cache false > crf.txt && (
             for /f "tokens=1,2,3 delims= " %%a in ('findstr /c:"crf" crf.txt') do (
@@ -61,7 +65,6 @@ for %%f in (*.mp4) do (
         ffmpeg -r %tf% -i output_frames/%%08d.png -vcodec %enc% -preset %pre% -crf !crf_value! -pix_fmt yuv420p10le "%%~nf_new.mkv" 2> NUL
         del "%%~nf_prerendered.mp4"
     ) else (
-        echo Analyzing Frames: %%f -- !currentfilenum! / !filecounter!
         ab-av1 crf-search -i "%%f" -e %enc% --preset %pre% --min-vmaf %5 --max-crf 45 --cache false > crf.txt && (
             for /f "tokens=1,2,3 delims= " %%a in ('findstr /c:"crf" crf.txt') do (
                 if "%%a"=="crf" (
@@ -87,5 +90,5 @@ echo Concatenating
 ffmpeg -safe 0 -f concat -i list.txt -c copy concat.mp4 2> NUL
 cd ..
 ffmpeg -i "%current_date%_splitted_video/concat.mp4" -i "%current_date%_splitted_video/%~n1.opus" -c copy "%~n1_final.mp4" 2> NUL
+rd /s /q "%current_date%_splitted_video"
 echo Finished
-pause
